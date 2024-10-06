28d9a41b91ddc8281b54336f1c9bb6f659e48292
Rich Powell <richardpowellofficial@yahoo.com>
2024-10-06 13:31:09 -0400

    CSS & HTML for percentage form

---
+++ script.js
@@ -0,0 +1,24 @@
+//Percentage Calculator Variables
+const percentOf = document.getElementById("percent-of");
+const percentOfWhole = document.getElementById("percent-of-whole");
+const isPercentOf = document.getElementById("is-percent-of");
+const isPercentOfWhole = document.getElementById("is-percent-of-whole");
+const percentIncreaseFrom = document.getElementById("percent-increase-from");
+const percentIncreaseTo = document.getElementById("percent-increase-to");
+const firstPercentageResult = document.getElementById("first-percentage-result");
+const secondPercentageResult = document.getElementById("second-percentage-result");
+const thirdPercentageResult = document.getElementById("third-percentage-result");
+const firstPercentCalculateBtn = document.getElementById("first-percentage-calculate-btn");
+const secondPercentCalculateBtn = document.getElementById("second-percentage-calculate-btn");
+const thirdPercentCalculateBtn = document.getElementById("third-percentage-calculate-btn");
+const firstPercentResetBtn = document.getElementById("first-percentage-reset-btn");
+const secondPercentResetBtn = document.getElementById("second-percentage-reset-btn");
+const thirdPercentResetBtn = document.getElementById("third-percentage-reset-btn");
+
+//Spread Calculator Variables
+const spreadSelector = document.getElementByIdById("spread-selector");
+
+
+firstPercentCalculateBtn.addEventListener("click", () => {
+    
+});
